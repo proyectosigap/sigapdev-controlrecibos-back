efb5606a8ef90236226f8f546c49dacfb354b611
proyectosigap <zmamanir@unmsm.edu.pe>
2020-08-22 14:52:55 -0500

    version 1.4.4

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -5,7 +5,7 @@ let options = {
 
 let pgp = require('pg-promise')(options);
 
-const  urlconnection = 'postgres://modulo4:modulo4@67.205.143.180:5432/tcs6';
+const  urlconnection = 'postgres://modulo4:modulo4@67.205.143.180:5432/tcs7';
 
 let cn = pgp(urlconnection);
 
